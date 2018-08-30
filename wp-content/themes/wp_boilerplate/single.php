40b1529022c02cf3c29174bdfff42606da32ac9f
Cristobal Dominguez <cristobald@gmail.com>
2018-08-30 19:53:16 -0300

    20 Final Commit

--- wp-content/themes/wp_boilerplate/single.php
+++ wp-content/themes/wp_boilerplate/single.php
@@ -3,11 +3,13 @@
 <?php if ( have_posts() ) { ?>
 	<?php while ( have_posts() ) { ?>
 		<?php the_post(); ?>
-
-		<?php the_post_thumbnail() ?>
+		<div class="jumbotron"></div>
+		<div class="container products__single">
+		<?php the_post_thumbnail(array(350,350)) ?>
 		<?php the_title() ?>
 		<time datetime="<?php the_time('Y-m-d') ?>"><?php the_time('d \d\e F \d\e Y') ?></time>
 		<?php the_content() ?>
+		</div>
 
 	<?php } ?>
 <?php } else { ?>
